e534790c590018906f031e6942fe7be9b6276bf0
João Rodrigues <78387984+joaopr03@users.noreply.github.com>
2023-01-06 11:10:34 +0000

    Update asa2_project.cpp

--- asa2_project.cpp
+++ asa2_project.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-int n_edges;
-int n_bows;
+int n_vertices = 0;
+int n_bows = 0;
 int n_max_trades = 0;
 
 // Class edge refers to two vertices of the graph that are connected, and the
@@ -60,14 +60,14 @@ class Graph {
         }
 
         void addEdge(int v1, int v2, int w) {
-            Edge edge(v1, v2, w);
+            Edge edge = Edge(v1, v2, w);
             edges.push_back(edge);
         }
 
         // From now on, there is an implementation of the Kruskal's algorithm,
         // which finds the minimum spanning tree of an undirected weighted
         // graph. Instead of searching for the lowest weight edges, which is
-        // the most common application of this algorith, we implemented it to 
+        // the most common application of this algorithm, we implemented it to 
         // get those who have the most weight, in order to get the shorter
         // path with the most trades possible between regions.
 
@@ -85,29 +85,33 @@ class Graph {
 
         void getMaxSpannigTree() {
             int size_edges = edges.size();
+            int number_of_important_edges = 0;
 
             sort(edges.begin(), edges.end(), compare);
 
             int *subset = new int[vertices];
-            memset(subset, -1, sizeof(int) * vertices);
+            memset(subset, -1, sizeof(int) * (vertices + 1));
 
             for (int i = 0; i < size_edges; i++) {
                 int v1 = search(subset, edges[i].getSource());
                 int v2 = search(subset, edges[i].getDestiny());
 
                 if(v1 != v2){
+                    number_of_important_edges++;
                     n_max_trades += edges[i].getWeight();
                     join(subset,v1,v2);
                 }
+                if(number_of_important_edges == n_vertices - 1)
+                    break;
             }
         }
 };
 
 // Function that reads the input and creates the graph
 Graph readInput() {
-    scanf("%d", &n_edges);
+    scanf("%d", &n_vertices);
     scanf("%d", &n_bows);
-    Graph graph = Graph(n_edges);
+    Graph graph = Graph(n_vertices);
     for(int i = 0; i < n_bows; i++){
         int s, d, w;
         scanf("%d", &s);
